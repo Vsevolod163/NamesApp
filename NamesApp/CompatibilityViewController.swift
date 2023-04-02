13fb984cf7125b687623a235df82ef95746cb51f
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-02 21:15:05 +0300

    Logic of program added

--- NamesApp/CompatibilityViewController.swift
+++ NamesApp/CompatibilityViewController.swift
@@ -14,16 +14,65 @@ final class CompatibilityViewController: UIViewController {
     
     @IBOutlet var backButton: UIButton!
     
+    @IBOutlet var compatibilityPV: UIProgressView!
+    
     var firstName: String!
     var secondName: String!
-    
-    @IBOutlet var compatibilityPV: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         backButton.layer.cornerRadius = 10
-        
+        let resultValue = findResult()
         namesLabel.text = "\(firstName ?? "") and \(secondName ?? "")"
+        
+        percentLabel.text = resultValue.formatted(.percent)
+        compatibilityPV.progressTintColor = resultValue < 50 ? .red : .green
+        compatibilityPV.progress = Float(resultValue) / 100
     }
+    
+    private func getValueOf(name: String) -> Int {
+        var count = 0
+        
+        for character in name.lowercased() {
+            switch character {
+            case "a", "i", "j", "q", "y":
+                count += 1
+            case "b", "k", "r":
+                count += 2
+            case "c", "g", "l", "s":
+                count += 3
+            case "d", "m", "t":
+                count += 4
+            case "e", "h", "n", "x":
+                count += 5
+            case "u", "v", "w":
+                count += 6
+            case "o", "z":
+                count += 7
+            case "f", "p":
+                count += 8
+            default:
+                count += 0
+            }
+        }
+        
+        return count
+    }
+    
+    private func findResult() -> Int {
+        var result = 0
+
+        let firstValue = Double(getValueOf(name: firstName))
+        let secondValue = Double(getValueOf(name: secondName))
+      
+        if firstValue < secondValue {
+            result = Int(firstValue / secondValue * 100)
+        } else {
+            result = Int(secondValue / firstValue * 100)
+        }
+    
+        return result
+    }
+    
 }
