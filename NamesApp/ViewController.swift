b4b9d77ec8ee4d090797e1c99f3e98e13373a420
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-02 18:50:37 +0300

    Initial Commit

---
+++ NamesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamesApp
+//
+//  Created by Vsevolod Lashin on 02.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
